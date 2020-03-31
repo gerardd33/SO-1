cf417cf9abd94b1a8940c9aaad9c83ebee26a0cd
gerardd33 <gerardd@interia.pl>
2020-03-31 22:30:05 +0200

    submitted version

--- dcl.asm
+++ dcl.asm
@@ -1,88 +1,15 @@
-%define l_var r13b
-%define r_var r12b
-
 SYS_EXIT equ 60
 SYS_WRITE equ 1
 SYS_READ equ 0
 STDIN equ 0
 STDOUT equ 1
-NL equ 10 ; newline
 
 ALPHABET_SIZE equ 42
 ASCII_SIZE equ 256
 BUFF_SIZE equ 4096
 
-; DEBUG
-
-%macro printVal 1
-    mov rax, %1
-%%printRAX:
-    mov rcx, digitSpace
-;   mov rbx, 10
-;   mov [rcx], rbx
-;   inc rcx
-    mov [digitSpacePos], rcx
- 
-%%printRAXLoop:
-    mov rdx, 0
-    mov rbx, 10
-    div rbx
-    push rax
-    add rdx, 48
- 
-    mov rcx, [digitSpacePos]
-    mov [rcx], dl
-    inc rcx
-    mov [digitSpacePos], rcx
-   
-    pop rax
-    cmp rax, 0
-    jne %%printRAXLoop
- 
-%%printRAXLoop2:
-    mov rcx, [digitSpacePos]
- 
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, rcx
-    mov rdx, 1
-    syscall
- 
-    mov rcx, [digitSpacePos]
-    dec rcx
-    mov [digitSpacePos], rcx
- 
-    cmp rcx, digitSpace
-    jge %%printRAXLoop2
- 
-%endmacro
-
-; print a zero-terminated string, without knowing its length
-%macro printStringZero 1 
-    mov rax, %1
-    add rax, 49 ; USUN POTEM
-    mov [printSpace], rax
-    mov rbx, 0
-%%printLoop:
-    mov cl, [rax]
-    cmp cl, 0
-    je %%endPrintLoop
-    inc rbx
-    inc rax
-    jmp %%printLoop
-%%endPrintLoop:
-    mov rax, SYS_WRITE
-    mov rdi, STDIN
-    mov rsi, [printSpace]
-    mov rdx, rbx
-    syscall
-%endmacro
-
-; END DEBUG
-
-
-
-
+%define l_var r13b
+%define r_var r12b
 
 %macro exit 1
 	mov rax, SYS_EXIT
@@ -122,8 +49,6 @@ BUFF_SIZE equ 4096
 
 ; take the argument from stack and copy it to %1
 %macro getArgFromStack 1
-	; TODO: wczytaj troche wiecej zeby sprawdzic czy
-	; argument nie jest postaci OKEJ + cos za duzo
 	pop rsi
 	mov rcx, ALPHABET_SIZE + 5
 	mov rdi, %1
@@ -158,7 +83,7 @@ BUFF_SIZE equ 4096
 %endmacro
 
 %macro triplicate 1
-	mov rax, 0
+	xor rax, rax
 %%triplicate_loop:
 	; copy the current element to positions ALPHABET_SIZE and
 	; 2 * ALPHABET_SIZE further 
@@ -175,12 +100,12 @@ BUFF_SIZE equ 4096
 ; %1 - array, %2 - place for array^-1
 %macro getInvAndValidate 2
 	mov r8, %1 ; a pointer to the current element in the permutation (A)
-	mov r9b, 0 ; index of the current element (idA)
+	xor r9b, r9b ; index of the current element (idA)
 %%getInvLoop:
 	validateChar byte [r8]
 	
 	; *A -= '1'
-	mov rax, 49
+	mov rax, '1'
 	sub [r8], rax ; *A -= '1'
 	
 	; invA[*A] != 0 - a repeating character
@@ -206,46 +131,55 @@ BUFF_SIZE equ 4096
 %macro validateKey 0
 	validateChar l_var
 	validateChar r_var
-	sub l_var, 49
-	sub r_var, 49
+	sub l_var, '1'
+	sub r_var, '1'
 %endmacro
 
 %macro validateT 0
-	; check if T == T^-1
-	mov r8, 0 ; index in the array(s)
-	mov r9, 0 ; result
-%%loopT1:
-	mov al, byte [invT + r8]
-	mov rbx, 1
-	cmp byte [prmT + r8], al
-	cmove r9, rbx
+	xor r8, r8
+%%loop1T:
+	mov al, ALPHABET_SIZE
+	sub [prmT + r8], al
 	
 	inc r8
-	cmp byte [prmT + r8], 0
-	jne %%loopT1
-	
-	cmp r9, 1
+	cmp r8, 3 * ALPHABET_SIZE
+	jne %%loop1T
+
+	xor r8, r8
+%%loop2T:
+	; T[i] != i
+	cmp byte [prmT + r8], r8b
 	je exit_failed
 	
-	; check if there are fixed points in the permutation
-	mov r8, 0
-%%loopT2:
-	cmp [prmT + r8], r8
-	je exit_failed
+	; T[T[i]] = i
+	movzx r9, byte [prmT + r8]
+	cmp byte [prmT + r9], r8b
+	jne exit_failed
+	
 	inc r8
-	jmp %%loopT2
+	cmp r8, ALPHABET_SIZE
+	jne %%loop2T
+	
+	xor r8, r8
+%%loop3T:
+	mov al, ALPHABET_SIZE
+	add [prmT + r8], al
+	
+	inc r8
+	cmp r8, 3 * ALPHABET_SIZE
+	jne %%loop3T
 	
 %endmacro
-	
+
 %macro moveRotors 0
 	inc r_var
 	modulo r_var, ALPHABET_SIZE
 	
-	cmp r_var, 76 - 49 ; 'L' - '1'
+	cmp r_var, 'L' - '1' 
 	je incrementLVar
-	cmp r_var, 82 - 49 ; 'R' - '1'
+	cmp r_var, 'R' - '1' 
 	je incrementLVar
-	cmp r_var, 84 - 49 ; 'T' - '1'
+	cmp r_var, 'T' - '1' 
 	je incrementLVar
 
 %endmacro
@@ -266,9 +200,7 @@ BUFF_SIZE equ 4096
 	syscall
 %endmacro
 
-
 %macro processInput 0
-papiez:
 %%blocLoop:
 	readBlocToBuffer
 	; check how many bytes have been read 
@@ -285,13 +217,12 @@ papiez:
 	je %%endElementLoop
 	
 	;validateChar byte [r8]
-	mov al, 49
+	mov al, '1'
 	sub [r8], al ; *cur -= '1'
 	
 	moveRotors
 comeBack:
 	
-	pap2:
 	; encipherment of the element
 	add [r8], r_var ; *cur += r, Qr
 	movzx rbx, byte [r8]
@@ -322,7 +253,6 @@ comeBack:
 	sub [r8], r_var ; * cur -= r, Qr^-1
 	
 	modulo [r8], ALPHABET_SIZE
-	; CZY NA PEWNO SIE ZMIENIA ZNAK W BUFF
 	mov al, 49
 	add [r8], al; convert to ascii again to write the element
 	inc r8
@@ -335,38 +265,17 @@ comeBack:
 %%allProcessed:
 %endmacro
 
-
 section .bss
-	; DEBUG
-	digitSpace resb 100
-    digitSpacePos resb 8
-    printSpace resb 8
-    ; END DEBUG
 	argc resb 8 ; number of arguments 
-	; TODO: powieksz ich rozmiary do walidacji
 	prmL resb 3 * ALPHABET_SIZE + 1 ; permutation L
 	prmR resb 3 * ALPHABET_SIZE + 1; permutation R
 	prmT resb 3 * ALPHABET_SIZE + 1; permutation T
 	invL resb 3 * ALPHABET_SIZE + 1 ; L^-1
 	invR resb 3 * ALPHABET_SIZE + 1; R^-1
 	invT resb 3 * ALPHABET_SIZE + 1; T^-1
-	buff resb BUFF_SIZE
 	bytesRead resb 8 ; to the buffer
-	tmpStr resb 2
-	
-    
-    
-section .data
-	; DEBUG
-	textArgument db "Argument #",0
-	colon db ": ",0
-	colonLen equ $ - colon
-	newline db 10,0
-	
-	; END DEBUG
+	buff resb BUFF_SIZE
 
-	
-	
 section .text
 	global _start
 	
@@ -376,7 +285,6 @@ _start:
 	getInvAndValidate prmR, invR
 	getInvAndValidate prmT, invT
 	
-	validateT
 	validateKey
 	
 	triplicate prmL
@@ -386,33 +294,15 @@ _start:
 	triplicate invR
 	triplicate invT
 	
-before:	
+	validateT
 	processInput
 	
-	;printStringZero prmL
-	;printStringZero newline
-	;printStringZero prmR
-	;printStringZero newline
-	;printStringZero prmT
-	;printStringZero newline
 	exit 0
 
 exit_failed:
 	exit 1
 	
 incrementLVar:
-inccccd:
 	inc l_var
 	modulo l_var, ALPHABET_SIZE
 	jmp comeBack
-
-
-	
-	
-; jak testowac 
-; movzx rax, bl
-; sub rax, 49
-; add [prmL + 1], rax
-; printStringZero prmL
-
-
